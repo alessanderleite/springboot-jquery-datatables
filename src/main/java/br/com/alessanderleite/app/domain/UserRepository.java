7d8c907b03acaaf42db0687cf699b23d52856ad2
alessanderleite <alessander.logistica@gmail.com>
2019-08-05 22:58:37 -0300

    create the interface named UserRepository.java

---
+++ src/main/java/br/com/alessanderleite/app/domain/UserRepository.java
@@ -0,0 +1,7 @@
+package br.com.alessanderleite.app.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Long>{
+	User findByUsername(String username);
+}
