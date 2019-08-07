d42052f4d9bf42f1ae9aa6895206e545d25a7e4e
alessanderleite <alessander.logistica@gmail.com>
2019-08-06 22:19:59 -0300

    create the controller class named TodoController.java

---
+++ src/main/java/br/com/alessanderleite/app/web/TodoController.java
@@ -0,0 +1,43 @@
+package br.com.alessanderleite.app.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import br.com.alessanderleite.app.domain.Todo;
+import br.com.alessanderleite.app.domain.TodoRepository;
+
+@Controller
+public class TodoController {
+
+	@Autowired
+	private TodoRepository repository;
+	
+	@RequestMapping(value = "/login")
+	public String login() {
+		return "login";
+	}
+	
+	// Show all todos
+	@RequestMapping(value = "/todos")
+	public String todoList(Model model) {
+		model.addAttribute("todolist", repository.findAll());
+		return "todos";
+	}
+	
+	// Add new todo
+	@RequestMapping(value = "/add")
+	public String addTodo(Model model) {
+		model.addAttribute("todo", new Todo());
+		return "addtodo";
+	}
+	
+	// Save new todo
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public String save(Todo todo) {
+		repository.save(todo);
+		return "redirect:todos";
+	}
+}
