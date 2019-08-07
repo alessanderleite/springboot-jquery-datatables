5bcd7527ecf09fa565a9250bfcfa8ab8012757cb
alessanderleite <alessander.logistica@gmail.com>
2019-08-07 12:36:03 -0300

    add a new method named getTodos()

--- src/main/java/br/com/alessanderleite/app/web/TodoController.java
+++ src/main/java/br/com/alessanderleite/app/web/TodoController.java
@@ -1,8 +1,11 @@
 package br.com.alessanderleite.app.web;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -39,5 +42,10 @@ public class TodoController {
 	public String save(Todo todo) {
 		repository.save(todo);
 		return "redirect:todos";
+	}
+	
+	@RequestMapping(value = "/gettodos")
+	public List<Todo> getTodos(@RequestBody Model model) {
+		return (List<Todo>) repository.findAll();
 	}
 }
